ead2d99e383447b11b946fa8c013c92845eb31bb
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-06-27 18:15:31 +0200

    What

--- scripts/libraries/geos_3.9.1.sh
+++ scripts/libraries/geos_3.9.1.sh
@@ -5,8 +5,9 @@ OUTPUT_LIBRARY_NAME="geos-${LIBRARY_VERSION}-heroku.tar.gz"
 
 curl -O https://ftp.osuosl.org/pub/osgeo/download/geos/geos-${LIBRARY_VERSION}.tar.bz2 \
   && tar -xjvf geos-${LIBRARY_VERSION}.tar.bz2 \
-  && cd geos-${LIBRARY_VERSION} \
-  && ./autogen.sh --prefix=${HEROKU_VENDOR_DIR} \
-  && make && make install \
+  && mkdir build \
+  && cd build \
+  && cmake ../geos-${LIBRARY_VERSION} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${HEROKU_VENDOR_DIR} \
+  && make -j6 && make install \
   && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/${OUTPUT_LIBRARY_NAME} . \
   && (cd ${TARGET_DIR} && exec md5sum ${OUTPUT_LIBRARY_NAME} > ${OUTPUT_LIBRARY_NAME}.md5)
