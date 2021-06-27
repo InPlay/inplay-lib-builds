f19d68d095a2a5867da4247e0d1a6c14087fc56d
Geremia Taglialatela <tagliala.dev@gmail.com>
2021-06-27 15:07:20 +0200

    Add -j4 option

--- scripts/libraries/geos_3.9.1.sh
+++ scripts/libraries/geos_3.9.1.sh
@@ -7,6 +7,6 @@ curl -O https://ftp.osuosl.org/pub/osgeo/download/geos/geos-${LIBRARY_VERSION}.t
   && tar -xjvf geos-${LIBRARY_VERSION}.tar.bz2 \
   && cd geos-${LIBRARY_VERSION} \
   && ./configure --prefix=${HEROKU_VENDOR_DIR} \
-  && make && make install \
+  && make -j4 && make install \
   && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/${OUTPUT_LIBRARY_NAME} . \
   && (cd ${TARGET_DIR} && exec md5sum ${OUTPUT_LIBRARY_NAME} > ${OUTPUT_LIBRARY_NAME}.md5)
--- scripts/libraries/proj_8.0.1.sh
+++ scripts/libraries/proj_8.0.1.sh
@@ -13,6 +13,6 @@ curl -O https://ftp.osuosl.org/pub/osgeo/download/proj/proj-${LIBRARY_VERSION}.t
   && tar -xvf proj-${LIBRARY_VERSION}.tar.gz \
   && cd proj-${LIBRARY_VERSION} \
   && ./configure --prefix=${HEROKU_VENDOR_DIR} \
-  && make && make install \
+  && make -j4 && make install \
   && tar -C ${HEROKU_VENDOR_DIR} -czvf ${TARGET_DIR}/${OUTPUT_LIBRARY_NAME} . \
   && (cd ${TARGET_DIR} && exec md5sum ${OUTPUT_LIBRARY_NAME} > ${OUTPUT_LIBRARY_NAME}.md5)
